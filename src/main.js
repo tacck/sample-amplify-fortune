f3cd66cc7a55b0625054e3ee956285c84447ed75
Kihara, Takuya <gray@tacck.net>
2021-02-08 14:53:40 +0900

    add amplify ui component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import '@aws-amplify/ui-vue'
+import Amplify from 'aws-amplify'
+import awsconfig from './aws-exports'
 
+Amplify.configure(awsconfig)
 Vue.config.productionTip = false
 
 new Vue({
